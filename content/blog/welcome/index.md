385ad763b06ab2dc2427c6b6abd0da8fbdd51618
Brett Baisley <26395611+brettbaisley@users.noreply.github.com>
2022-11-13 14:55:32 -0500

    Gatsby v5 upgrade

---
+++ content/blog/welcome/index.md
@@ -0,0 +1,9 @@
+---
+title: The Bloginator
+date: "2022-11-07T21:00:00.000Z"
+description: Welcome to my blog! Why am I writing a blog? I don't know. Seems everyone has a blog, or a podcast or a YouTube channel, and I was suffering from FOMO.
+---
+
+Welcome to my blog! Why am I writing a blog? I don't know. Seems everyone has a blog, or a podcast or a YouTube channel, and I was suffering from FOMO. Nah, the real reason why I started a blog was to learn more about Gatsby, and I figured, why not start with a blog. Seemed like a good usecase at the time.
+
+But then it dawned no me, I'm not much of a writer. This post is likely filled with typos and grammer mistakes. I don't post to social media. So why would anyone want to read my blog? All I can offer is a little bit of amuzement and laughter regardin this crazy world around us. I won't be serious. I avoid controversy as life is too short to be arguing with people all the time. If you don't like what I say, don't read it, I will not be offended. If you are ok with those rules, then welcome, and enjoy the articles.
---
+++ gatsby-config.js
@@ -0,0 +1,53 @@
+module.exports = {
+    siteMetadata: {
+      title: `Brett's Blog`,
+      author: {
+        name: `Brett Baisley`,
+        summary: `who lives and works in New York.`,
+      },
+      description: `The personal blog of Brett Baisley`,
+      siteUrl: `https://blog.brettbaisley.com`,
+    },
+    plugins: [
+      `gatsby-plugin-image`,
+      {
+        resolve: `gatsby-source-filesystem`,
+        options: {
+          path: `${__dirname}/content/blog`,
+          name: `blog`,
+        },
+      },
+      {
+        resolve: `gatsby-source-filesystem`,
+        options: {
+          name: `images`,
+          path: `${__dirname}/src/images`,
+        },
+      },
+      {
+        resolve: `gatsby-transformer-remark`,
+        options: {
+          plugins: [
+            {
+              resolve: `gatsby-remark-images`,
+              options: {
+                maxWidth: 630,
+              },
+            },
+            {
+              resolve: `gatsby-remark-responsive-iframe`,
+              options: {
+                wrapperStyle: `margin-bottom: 1.0725rem`,
+              },
+            },
+            `gatsby-remark-prismjs`,
+            `gatsby-remark-copy-linked-files`,
+            `gatsby-remark-smartypants`,
+          ],
+        },
+      },
+      `gatsby-transformer-sharp`,
+      `gatsby-plugin-sharp`,
+    ],
+  }
+  
